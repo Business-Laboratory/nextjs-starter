c1141ef453a1bf45ff8fcf96950900118e8897a0
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-09-30 09:44:24 -0500

    Add tailwind and other stuff

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['react-app', 'plugin:jsx-a11y/recommended'],
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+}
---
+++ pages/my-page.js
@@ -0,0 +1,3 @@
+export default function () {
+  return <h2 className='text-3xl'>It's me!</h2>
+}
