236543e97bb65c2efe681802e3415eea710fa7fc
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-12-03 13:58:13 -0600

    Update dependencies, configs, and refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,40 +2,38 @@ import clsx from 'clsx'
 import Head from 'next/head'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-/** @jsx jsx */
-import { jsx, css } from '@emotion/core'
 
 export default function Home() {
   const [showSidebar, setShowSidebar] = useState(false)
 
   return (
-    <div className='flex flex-col h-screen'>
+    <div className="flex flex-col h-screen">
       <Head>
         <title>Layout examples</title>
       </Head>
 
       <Header>
         <button
-          className='ml-2'
+          className="ml-2"
           onClick={() => setShowSidebar((prev) => !prev)}
         >
           {showSidebar ? (
-            <CloseIcon aria-label='Close sidebar' />
+            <CloseIcon aria-label="Close sidebar" />
           ) : (
-            <HamburgerIcon aria-label='Open sidebar' />
+            <HamburgerIcon aria-label="Open sidebar" />
           )}
         </button>
-        <Link href='/'>
-          <a className='font-display font-bold text-2xl text-gray-red-100 ml-2'>
+        <Link href="/">
+          <a className="ml-2 text-2xl font-bold font-display text-gray-red-100">
             Home
           </a>
         </Link>
         {/* TODO: add avatar */}
       </Header>
 
-      <div className='flex-1 flex flex-row overflow-hidden'>
+      <div className="flex flex-row flex-1 overflow-hidden">
         <Sidebar showSidebar={showSidebar}>
-          <div className='space-y-8 text-gray-yellow-100 pl-4 pt-2'>
+          <div className="pt-2 pl-4 space-y-8 text-gray-yellow-100">
             <p>Content</p>
             <p>Content</p>
             <p>Content</p>
@@ -65,7 +63,7 @@ export default function Home() {
 
         <Main>
           <article>
-            <section className='space-y-8 pl-4 pt-2'>
+            <section className="pt-2 pl-4 space-y-8">
               <p>Content</p>
               <p>Content</p>
               <p>Content</p>
@@ -121,18 +119,18 @@ function HamburgerIcon(props: React.SVGAttributes<SVGElement>) {
   const { className, ...rest } = props
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       className={clsx('w-6 h-6 text-gray-red-100', className)}
       {...rest}
     >
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
+        strokeLinecap="round"
+        strokeLinejoin="round"
         strokeWidth={2}
-        d='M4 6h16M4 12h16M4 18h16'
+        d="M4 6h16M4 12h16M4 18h16"
       />
     </svg>
   )
@@ -141,18 +139,18 @@ function CloseIcon(props: React.SVGAttributes<SVGElement>) {
   const { className, ...rest } = props
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       className={clsx('w-6 h-6 text-gray-red-100', className)}
       {...rest}
     >
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
+        strokeLinecap="round"
+        strokeLinejoin="round"
         strokeWidth={2}
-        d='M6 18L18 6M6 6l12 12'
+        d="M6 18L18 6M6 6l12 12"
       />
     </svg>
   )
@@ -189,12 +187,12 @@ function Sidebar({ showSidebar, className, children }: SidebarProps) {
 
   return (
     <aside
-      css={css`
-        width: 16rem;
-        margin-left: ${showSidebar ? 0 : -16}rem;
-        transition: margin-left 0.5s ease-in-out;
-        visibility: ${animationStatus === 'closed' ? 'hidden' : 'visible'};
-      `}
+      // css={css`
+      //   width: 16rem;
+      //   margin-left: ${showSidebar ? 0 : -16}rem;
+      //   transition: margin-left 0.5s ease-in-out;
+      //   visibility: ${animationStatus === 'closed' ? 'hidden' : 'visible'};
+      // `}
       className={clsx(
         'static h-screen inset-y-0 left-0 bg-matisse-blue-400 overflow-y-auto',
         className
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,80 +3,80 @@ const md = '720px'
 const lg = '1440px'
 
 module.exports = {
-  purge: ['src/**/*.tsx'],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
       display: ['Poppins', 'sans-serif'],
       // Note, Work Sans must be quoted per the docs: https://tailwindcss.com/docs/font-family/#font-families
-      body: ['"Work Sans"', 'sans-serif']
+      body: ['"Work Sans"', 'sans-serif'],
     },
     screens: { sm, md, lg },
     extend: {
       width: { sm, md, lg },
       minWidth: (theme) => ({
         auto: 'auto',
-        ...theme('spacing')
+        ...theme('spacing'),
       }),
       colors: {
         'matisse-red': {
           100: '#FF6356',
           200: '#E84234',
           300: '#C72A27',
-          400: '#7B1F22'
+          400: '#7B1F22',
         },
         copper: {
           100: '#FBD7B7',
           200: '#EABF9A',
           300: '#C9A17E',
-          400: '#AB855E'
+          400: '#AB855E',
         },
         'scout-green': {
           100: '#A4A16E',
           200: '#788D5E',
           300: '#58623F',
-          400: '#34483C'
+          400: '#34483C',
         },
         'lichen-green': {
           100: '#BFF5DC',
           200: '#B1E9CF',
           300: '#88B6B3',
-          400: '#478F8B'
+          400: '#478F8B',
         },
         'matisse-blue': {
           100: '#7697FF',
           200: '#5D77C7',
           300: '#435691',
-          400: '#131C25'
+          400: '#131C25',
         },
         'calico-orange': {
           100: '#FFC29F',
           200: '#E79669',
           300: '#DE7539',
-          400: '#D85C16'
+          400: '#D85C16',
         },
         'gray-pink': {
           100: '#FFF4F3',
           200: '#CAB4B2',
           300: '#998381',
-          400: '#302928'
+          400: '#302928',
         },
         'gray-yellow': {
           100: '#FFF9F0',
           200: '#D2C6B4',
           300: '#A49989',
-          400: '#2C2925'
+          400: '#2C2925',
         },
         'gray-red': {
           100: '#FFF5F3',
           200: '#DCBEBA',
           300: '#AE9390',
-          400: '#221D1D'
+          400: '#221D1D',
         },
         black: '#000000',
-        white: '#FFFFFF'
-      }
-    }
+        white: '#FFFFFF',
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 }
