2c0f59bf48992509f48766fad5d5ba6652d96a19
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-12-03 15:37:58 -0600

    Convert remaining pages to TS

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from 'next/app'
+
+import '../styles/index.css'
+
+type ComponentWithPageLayout = {
+  Component: AppProps['Component'] & {
+    PageLayout?: React.ComponentType
+  }
+}
+
+function App({ Component, pageProps }: AppProps & ComponentWithPageLayout) {
+  // get a page root if one was set
+  const PageLayout =
+    Component.PageLayout ||
+    (({ children }: { children: React.ReactNode }) => <>{children}</>)
+
+  return (
+    <PageLayout>
+      <Component {...pageProps} />
+    </PageLayout>
+  )
+}
+
+export default App
