aa038dea777fb3f45239bc5019c0d67c3b9a378a
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-03-30 14:04:25 -0500

    Updated packages and removed unnecessary stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -79,46 +79,6 @@ function Header({ className, children }: HeaderProps) {
   )
 }
 
-// taken from https://heroicons.com/
-function HamburgerIcon(props: React.SVGAttributes<SVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      tw="w-6 h-6 text-gray-yellow-100"
-      {...props}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M4 6h16M4 12h16M4 18h16"
-      />
-    </svg>
-  )
-}
-function CloseIcon(props: React.SVGAttributes<SVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      tw="w-6 h-6 text-gray-yellow-100"
-      {...props}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M6 18L18 6M6 6l12 12"
-      />
-    </svg>
-  )
-}
-
 type MainProps = {
   className?: string
   children?: React.ReactNode
