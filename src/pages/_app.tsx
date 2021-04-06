ac8ed8c1257f40a4ec5d665829e3ab6b4623eaed
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-04-06 17:09:50 -0500

    Remove bounciness for iOS and macOS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,16 +12,19 @@ function App({ Component, pageProps }: AppProps & ComponentWithPageLayout) {
   return (
     <>
       <GlobalStyles />
-      {
-        // get a page root if one was set
-        Component.PageLayout ? (
-          <Component.PageLayout>
+      {/* this style is applied to avoid the "bounce" on iOS/macOS: https://stackoverflow.com/a/21247262/10128987 */}
+      <div tw="absolute inset-0 overflow-auto">
+        {
+          // get a page root if one was set
+          Component.PageLayout ? (
+            <Component.PageLayout>
+              <Component {...pageProps} />
+            </Component.PageLayout>
+          ) : (
             <Component {...pageProps} />
-          </Component.PageLayout>
-        ) : (
-          <Component {...pageProps} />
-        )
-      }
+          )
+        }
+      </div>
     </>
   )
 }
